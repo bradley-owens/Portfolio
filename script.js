5c2d235ea08177193c71334898eb3f03e2815209
Bradley Owens <bradowens17@hotmail.com>
2022-05-14 13:27:51 +1000

    Add contact form send function to email

---
+++ script.js
@@ -0,0 +1,16 @@
+// Send Email function
+
+function sendEmail() {
+  let tempParameters = {
+    from_name: document.getElementById("name").value,
+    from_email: document.getElementById("email").value,
+    from_phone: document.getElementById("phone").value,
+    message: document.getElementById("message").value,
+  };
+
+  emailjs
+    .send("service_t0fv5m8", "template_2byut5t", tempParameters)
+    .then(function (response) {
+      console.log("sucess", response.status);
+    });
+}
