ca7b659f7554e7bf754e9b8cb03c45cc814115e1
Bradley Owens <bradowens17@hotmail.com>
2022-05-18 17:13:50 +1000

    Adding smooth scrolling using javascript but commenting out and keeping CSS method to make the code cleaner

--- script.js
+++ script.js
@@ -15,3 +15,20 @@ function sendEmail() {
       alert("Your message has been sent! I'll be in touch as soon as I can! ");
     });
 }
+
+// smooth scrolling option with javascript
+
+// const scrollToSection = document.querySelectorAll(".smooth_scroll");
+// const section2 = document.querySelector(".two");
+// const section3 = document.querySelector(".three");
+// const section4 = document.querySelector(".four");
+// const section5 = document.querySelector(".five");
+// const section6 = document.querySelector(".six");
+
+// for (let i = 0; i < scrollToSection.length; i++) {
+//   scrollToSection[i].addEventListener("click", function (e) {
+//     const num = scrollToSection[i].id;
+//     const clickedSection = `section${num}`;
+//     clickedSection.scrollIntoView({ behavior: "smooth" });
+//   });
+// }
