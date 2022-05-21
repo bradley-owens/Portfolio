64efb56132c310f4b4c19fbcc3a2f2842a61614c
Bradley Owens <bradowens17@hotmail.com>
2022-05-21 16:26:13 +1000

    add fade and highlight animation for navigation links

--- script.js
+++ script.js
@@ -45,3 +45,29 @@ scrollToLinks.addEventListener("click", function (e) {
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
+
+// changing opacity of links when mouse over
+
+const nav = document.querySelector(".nav");
+
+const hoverOver = function (e, opac) {
+  if (e.target.classList.contains("link-nav")) {
+    console.log("success");
+    const link = e.target;
+    const siblings = link.closest(".nav").querySelectorAll(".link-nav");
+
+    siblings.forEach((el) => {
+      if (el !== link) {
+        el.style.opacity = opac;
+      }
+    });
+  }
+};
+
+nav.addEventListener("mouseover", function (e) {
+  hoverOver(e, 0.5);
+});
+
+nav.addEventListener("mouseout", function (e) {
+  hoverOver(e, 1);
+});
