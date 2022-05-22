2922c269f269a14d1d1bbe29d529db138c2979df
Bradley Owens <bradowens17@hotmail.com>
2022-05-22 14:54:27 +1000

    add fade in animations for each section using javascript observer

--- script.js
+++ script.js
@@ -71,3 +71,24 @@ nav.addEventListener("mouseover", function (e) {
 nav.addEventListener("mouseout", function (e) {
   hoverOver(e, 1);
 });
+
+/// Revealing sections animation
+const allSections = document.querySelectorAll(".section");
+
+const revealSections = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section-hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSections, {
+  root: null,
+  threshold: 0.1,
+});
+
+allSections.forEach((section) => {
+  sectionObserver.observe(section);
+  section.classList.add("section-hidden");
+});
